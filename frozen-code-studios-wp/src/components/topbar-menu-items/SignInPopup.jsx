e990031e3f697ca0a1f24a5bb35c39962d208859
AgentCryo <ColtonCryo@gmail.com>
2024-02-27 19:10:01 -0600

    Added Create window

--- frozen-code-studios-wp/src/components/topbar-menu-items/SignInPopup.jsx
+++ frozen-code-studios-wp/src/components/topbar-menu-items/SignInPopup.jsx
@@ -3,6 +3,9 @@ import { useAuthContext } from '../../providers/AuthProvider';
 import './SignInPopup.css';
 
 export const SignInPopup = (props) => {
+
+  let Login_Create = false;
+
   const closePopup = props.closePopup;
   const { register } = useAuthContext();
 
@@ -16,26 +19,56 @@ export const SignInPopup = (props) => {
     console.log('Login Data:', loginData);
   };
 
-  return props.showPopup ? (
-    <div className='popup'>
-      <div className='sign-in-window'>
-        <div className='p2'>Username</div>
-        <input type='text' name='UserName' value={loginData.UserName} />
-        <div className='p2'>Gmail / Email</div>
-        <input type='text' name='Gmail' value={loginData.Gmail} />
-        <div className='p2'>Password</div>
-        <input type='password' name='Password' value={loginData.Password} />
-        <button onClick={PressedLogin}>Login</button>
-        <div className='p2' />
-        <button>Create Account</button>
-        <div className='p2' />
-        <button onClick={closePopup}>Cancel</button>
-        <div className='p2' />
+  if (!Login_Create) {
+    return props.showPopup ? (
+      <div className='popup'>
+        <div className='sign-in-window'>
+          <div className='p2'>Gmail / Email</div>
+          <input type='text' name='Gmail' value={loginData.Gmail} />
+
+          <div className='p2'>Password</div>
+          <input type='password' name='Password' value={loginData.Password} />
+
+          <button onClick={PressedLogin}>Login</button>
+          <div className='p2' />
+
+          <button>Create Account</button>
+          <div className='p2' />
+
+          <button onClick={closePopup}>Cancel</button>
+          <div className='p2' />
+        </div>
       </div>
-    </div>
-  ) : (
-    ''
-  );
+    ) : (
+      ''
+    );
+  } else {
+    return props.showPopup ? (
+      <div className='popup'>
+        <div className='sign-in-window'>
+          <div className='p2'>Username</div>
+          <input type='text' name='UserName' value={loginData.UserName} />
+
+          <div className='p2'>Gmail / Email</div>
+          <input type='text' name='Gmail' value={loginData.Gmail} />
+
+          <div className='p2'>Password</div>
+          <input type='password' name='Password' value={loginData.Password} />
+
+          <button onClick={PressedLogin}>Login</button>
+          <div className='p2' />
+
+          <button>Create Account</button>
+          <div className='p2' />
+          
+          <button onClick={closePopup}>Cancel</button>
+          <div className='p2' />
+        </div>
+      </div>
+    ) : (
+      ''
+    );
+  }
 };
 
 export const UGP_varification = (loginData) => {
