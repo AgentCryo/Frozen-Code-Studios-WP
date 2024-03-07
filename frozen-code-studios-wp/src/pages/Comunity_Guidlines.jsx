dad0019bf4d0f4ba3718be2c5402a75bc06112cb
AgentCryo <ColtonCryo@gmail.com>
2024-03-07 10:42:21 -0600

    More styling

--- frozen-code-studios-wp/src/pages/Comunity_Guidlines.jsx
+++ frozen-code-studios-wp/src/pages/Comunity_Guidlines.jsx
@@ -5,85 +5,109 @@ export const Comunity_Guidlines = () => {
         <div className="darkened-content">
           <div className="h3">Frozen Code Studios Discord Guidelines</div>
           <div className="text-container">
-            <div className="h2">&emsp;1. Be respectful:</div>
-            <div className="p1">- Treat everyone with respect and kindness.</div>
-            <div className="p1">
-              - Avoid offensive language, harassment, or discrimination based on race, gender,
-              religion, sexual orientation, or any other personal characteristics.
-            </div>
+            <li className="h2">
+              1. Be respectful:
+              <div className="p1">- Treat everyone with respect and kindness.</div>
+              <div className="p1">
+                - Avoid offensive language, harassment, or discrimination based on race, gender,
+                religion, sexual orientation, or any other personal characteristics.
+              </div>
+            </li>
 
-            <div className="h2">&emsp;2. No spamming:</div>
-            <div className="p1">- Avoid excessive messages, emojis, or repeated content.</div>
-            <div className="p1">
-              - Do not flood channels with irrelevant or off-topic information.
-            </div>
+            <li className="h2">
+              2. No spamming:
+              <div className="p1">- Avoid excessive messages, emojis, or repeated content.</div>
+              <div className="p1">
+                - Do not flood channels with irrelevant or off-topic information.
+              </div>
+            </li>
 
-            <div className="h2">&emsp;3. Keep it safe:</div>
-            <div className="p1">
-              - Do not share personal information, including addresses, phone numbers, or financial
-              details.
-            </div>
-            <div className="p1">
-              - Report any suspicious or inappropriate behavior to the moderators.
-            </div>
+            <li className="h2">
+              3. Keep it safe:
+              <div className="p1">
+                - Do not share personal information, including addresses, phone numbers, or
+                financial details.
+              </div>
+              <div className="p1">
+                - Report any suspicious or inappropriate behavior to the moderators.
+              </div>
+            </li>
 
-            <div className="h2">&emsp;4. Use appropriate channels:</div>
-            <div className="p1">- Keep discussions relevant to the channel's topic.</div>
-            <div className="p1">
-              - Use the appropriate channels for different types of content (e.g., general chat,
-              memes, announcements).
-            </div>
+            <li className="h2">
+              4. Use appropriate channels:
+              <div className="p1">- Keep discussions relevant to the channel's topic.</div>
+              <div className="p1">
+                - Use the appropriate channels for different types of content (e.g., general chat,
+                memes, announcements).
+              </div>
+            </li>
 
-            <div className="h2">&emsp;5. Respect privacy:</div>
-            <div className="p1">
-              - Do not share others' personal information without their explicit consent.
-            </div>
-            <div className="p1">- Respect the privacy of fellow community members.</div>
+            <li className="h2">
+              5. Respect privacy:
+              <div className="p1">
+                - Do not share others' personal information without their explicit consent.
+              </div>
+              <div className="p1">- Respect the privacy of fellow community members.</div>
+            </li>
 
-            <div className="h2">&emsp;6. Listen to moderators:</div>
-            <div className="p1">
-              - Follow the instructions and guidelines provided by the moderators.
-            </div>
-            <div className="p1">
-              - If you have concerns or questions, contact a moderator privately.
-            </div>
+            <li className="h2">
+              6. Listen to moderators:
+              <div className="p1">
+                - Follow the instructions and guidelines provided by the moderators.
+              </div>
+              <div className="p1">
+                - If you have concerns or questions, contact a moderator privately.
+              </div>
+            </li>
 
-            <div className="h2">&emsp;7. No trolling or flaming:</div>
-            <div className="p1">
-              - Avoid trolling, flaming, or any behavior intended to provoke negative reactions.
-            </div>
-            <div className="p1">
-              - Disagreements should be handled respectfully and constructively.
-            </div>
+            <li className="h2">
+              7. No trolling or flaming:
+              <div className="p1">
+                - Avoid trolling, flaming, or any behavior intended to provoke negative reactions.
+              </div>
+              <div className="p1">
+                - Disagreements should be handled respectfully and constructively.
+              </div>
+            </li>
 
-            <div className="h2">&emsp;8. English only (if applicable):</div>
-            <div className="p1">
-              - The server primarily uses English, keep communication in English to ensure
-              inclusivity.
-            </div>
+            <li className="h2">
+              8. English only (if applicable):
+              <div className="p1">
+                - The server primarily uses English, keep communication in English to ensure
+                inclusivity.
+              </div>
+            </li>
 
-            <div className="h2">&emsp;9. No advertising Without permission:</div>
-            <div className="p1">
-              - Do not promote external products, services, or other Discord servers without
-              permission from the server administrators.
-            </div>
+            <li className="h2">
+              9. No advertising Without permission:
+              <div className="p1">
+                - Do not promote external products, services, or other Discord servers without
+                permission from the server administrators.
+              </div>
+            </li>
 
-            <div className="h2">&emsp;10. Be mindful of spoilers:</div>
-            <div className="p1">
-              - Use spoiler tags when discussing sensitive information or details about books,
-              movies, or games.
-            </div>
+            <li className="h2">
+              10. Be mindful of spoilers:
+              <li className="p1">
+                - Use spoiler tags when discussing sensitive information or details about books,
+                movies, or games.
+              </li>
+            </li>
 
-            <div className="h2">&emsp;11. Stay on topic during events:</div>
-            <div className="p1">
-              - During events or specific discussions, try to stay on topic to enhance the overall
-              experience.
-            </div>
+            <li className="h2">
+              11. Stay on topic during events:
+              <div className="p1">
+                - During events or specific discussions, try to stay on topic to enhance the overall
+                experience.
+              </div>
+            </li>
 
-            <div className="h2">&emsp;12. Keep voice channels clear:</div>
-            <div className="p1">
-              - If a voice channel has a specific purpose, respect it and use it accordingly.
-            </div>
+            <li className="h2">
+              12. Keep voice channels clear:
+              <div className="p1">
+                - If a voice channel has a specific purpose, respect it and use it accordingly.
+              </div>
+            </li>
           </div>
         </div>
       </div>
